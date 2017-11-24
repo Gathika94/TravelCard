0bdd178cfe6aa64ae9830f29d9706728f694c709
Gathika94 <gathika.14@cse.mrt.ac.lk>
2017-11-25 00:58:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './CommentBox';
+import CardForm from './CardForm';
+
+ReactDOM.render(
+  <CardForm/>,
+  document.getElementById('root')
+);
